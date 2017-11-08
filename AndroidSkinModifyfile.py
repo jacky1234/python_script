ead2b9cecf7374dd83c2c0a71dca8e4973c43024
jacky1234 <jackdoson2011@gmail.com>
2017-11-08 16:21:38 +0800

    1. copy .9 png to sep folder

--- AndroidSkinModifyfile.py
+++ AndroidSkinModifyfile.py
@@ -1,8 +1,12 @@
 import os
+import shutil
 
 dir = input("请输入你要扫描的目录或者文件:")
 modify = []
 remove = []
+error = []
+exeFiles = []
+exe9Dir = "9Path"
 
 try:
     for f in os.listdir(dir):
@@ -14,17 +18,34 @@ try:
             # 源文件名字
             oldName = f
             newName = f.replace("@3x.png", ".png")
-
             modify.append(oldName + "->" + newName)
             os.rename(os.path.join(dir, oldName), os.path.join(dir, newName))
+        if f.find(" ") > 0:
+            error.append(f)
+        if f.find(".9.png") > 0:
+            exeFiles.append(f)
+
+    # delete dir if exist
+    if os.path.exists(os.path.join(dir, exe9Dir)):
+        shutil.rmtree(os.path.join(dir, exe9Dir))
+    os.makedirs(os.path.join(dir, exe9Dir))
+
+    print("copy .9 file to " + exe9Dir)
+    for f in exeFiles:
+        shutil.copy(os.path.join(dir, f), os.path.join(dir, exe9Dir, f))
 
     print("modify:")
     for m in modify:
         print(m)
+
     print("remove:")
     for r in remove:
         print(r)
 
+    print("error contain blank file list:")
+    for e in error:
+        print(e)
+
     print("success!")
-except OSError:
-    print("exception!")
+except OSError as error:
+    print("exception!:", error)
